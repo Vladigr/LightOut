cbec15f562c33b9f760dd476d854e2228efaef55
vlad <vladigr1@gmail.com>
2021-07-17 11:28:49 +0300

    ui: board size fix

--- app/src/main/java/com/example/lightout/boundary/TableButton.java
+++ app/src/main/java/com/example/lightout/boundary/TableButton.java
@@ -1,7 +1,12 @@
 package com.example.lightout.boundary;
 
 import android.content.Context;
+import android.content.res.Resources;
+import android.util.Log;
+import android.util.TypedValue;
 import android.widget.Button;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 
 import androidx.annotation.NonNull;
 
@@ -30,9 +35,22 @@ public class TableButton extends androidx.appcompat.widget.AppCompatButton {
 
     public TableButton(@NonNull Context context, int i, int j, boolean state) {
         super(context);
+        int pxSize = (int) dpToPixel(50);
+        Log.i("tableButton", "pxsize: " + pxSize);
+        this.setLayoutParams(new TableRow.LayoutParams(pxSize,pxSize));
         this.i =i;
         this.j =j;
         this.state = state;
     }
 
+    float dpToPixel(float dp){
+        // Converts 14 dip into its equivalent px
+        Resources r = getResources();
+        float px = TypedValue.applyDimension(
+                TypedValue.COMPLEX_UNIT_DIP,
+                dp,
+                r.getDisplayMetrics()
+        );
+        return px;
+    }
 }
